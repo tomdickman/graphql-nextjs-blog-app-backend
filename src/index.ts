76040f234dc8ce7ddf0b8e95e18fc1bab60111da
Tom Dickman <twdickman@gmail.com>
2022-03-08 10:19:18 +1100

    initial apollo server graphql API

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { ApolloServer } from "apollo-server"
+
+import typeDefs from "./schema"
+import { Query } from "./resolvers" 
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers: {
+        Query,
+    },
+})
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server is running on: ${url}`)
+})
